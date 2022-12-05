b109f8550324f3dc1e03337cd7aaa5bc1554a3c9
Nicholas Peters <peters.nicholas@gmail.com>
2022-12-05 10:26:36 +0100

    Name input on landing page

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
 import './style/style.scss';
 
-const user: string = 'Nicholas';
+// Landing page name input
+const landingPage = document.querySelector('#landingPage');
+const nameDisplay = document.querySelector('#user');
+const nameInput = document.querySelector('#nameInput') as HTMLInputElement;
+const nameSubmit = document.querySelector('#nameSubmit');
 
-const userNameDisplay = document.querySelector('#user');
+const todoInput = document.querySelector('#todoInput') as HTMLInputElement;
+const todoInputSubmit = document.querySelector('#todoInputSubmit');
 
-userNameDisplay.innerHTML = user;
+function getName() {
+  const user = nameInput.value;
+  if (nameDisplay != null) {
+    nameDisplay.innerHTML = user;
+  }
+  if (landingPage != null) {
+    landingPage.classList.add('hidden');
+  }
+}
+
+function addTodo() {
+  console.log(todoInput.value);
+}
+
+if (nameSubmit != null) {
+  nameSubmit.addEventListener('click', getName);
+}
+if (todoInputSubmit != null) {
+  todoInputSubmit.addEventListener('click', addTodo);
+}
