8cd0050ad2f99e0d44c5085eecf2a6ca1b6cc675
Nicholas Peters <peters.nicholas@gmail.com>
2022-12-06 20:10:38 +0100

    Added categories, working on menu

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,46 @@
 import './style/style.scss';
-import { gsap } from "gsap";
+import { gsap } from 'gsap';
 
 const landingPage = document.querySelector('#landingPage');
 const nameDisplay = document.querySelector('#user');
 const nameInput = document.querySelector('#nameInput') as HTMLInputElement;
 const nameSubmit = document.querySelector('#nameSubmit');
+const menuBtn = document.querySelector('#menuBtn') as HTMLButtonElement;
+const menu = document.querySelector('#menu');
 
 const todoInput = document.querySelector('#todoInput') as HTMLInputElement;
 const todoInputSubmit = document.querySelector('#todoInputSubmit');
+const categorySelect = document.querySelector('#categorySelect') as HTMLInputElement;
 const todoList = document.querySelector('#todoList');
 
-const todoArray: {
+class TodoItem {
+  category: string;
+
   index: number;
+
   todo: string;
-}[] = [];
+
+  constructor(category: string, index: number, todo: string) {
+    this.category = category;
+    this.index = index;
+    this.todo = todo;
+  }
+}
+
+const todoArray: TodoItem[] = [];
+
+function toggleMenu() {
+  let isOpen = false;
+  if (isOpen) {
+    gsap.from(menu, { x: -100, duration: 0.5 });
+    isOpen = false;
+  }
+  if (!isOpen) {
+    gsap.to(menu, { x: 100, duration: 0.5 });
+    isOpen = true;
+  }
+  // menu?.classList.toggle('visually-hidden');
+}
 
 function hide() {
   landingPage?.classList.add('hidden');
@@ -29,47 +56,54 @@ function getName() {
   }
 }
 
-function showTodo() {
+function showCategory(category: string) {
   let todoListHtml = '';
-
   if (todoArray.length === 0) {
     todoListHtml = '<p>Dont you have anything to do?! <br> Add something!</p>';
   } else {
-    todoArray.forEach((item, index) => {
-      item.index = index;
-      todoListHtml += `
-      <div class="list-whole" data-id="${index}">
-        <div class="list-left">
-          <input type="checkbox" id="completed">
-          <p>${item.todo}</p>
+    todoArray.forEach((item) => {
+      if (item.category === category) {
+        todoListHtml += `
+        <div class="list-whole" data-id="${item.index}">
+          <div class="list-left">
+            <input type="checkbox" id="completed">
+            <p>${item.todo}</p>
+          </div>
+          <div class="list-right">
+            <span class="material-symbols-outlined"><button class="delTodo">
+            delete
+            </button></span>
+          </div>
         </div>
-        <div class="list-right">
-          <span class="material-symbols-outlined"><button class="delTodo">
-          delete
-          </button></span>
-        </div>
-      </div>
-      `;
+        `;
+      }
     });
   }
-
   if (todoList != null) {
     todoList.innerHTML = todoListHtml;
   }
 }
 
+function showTodo() {
+  const selectedCategory = categorySelect.value;
+  showCategory(selectedCategory);
+}
+
 function addTodo() {
   if (todoInput.value !== '') {
     const todoValue = todoInput.value;
-    const newTodo = { todo: todoValue };
+    const todoCategory = categorySelect.value;
+
+    const newTodo = new TodoItem(todoCategory, todoArray.length, todoValue);
     todoArray.push(newTodo);
+
     showTodo();
     todoInput.value = '';
   }
 }
 
 function deleteTodo() {
-  const index = event.target.parentElement.parentElement.parentElement.dataset.id;
+  const index: number = event.target.parentElement.parentElement.parentElement.dataset.id;
   todoArray.splice(index, 1);
   showTodo();
 }
@@ -78,7 +112,7 @@ nameSubmit?.addEventListener('click', getName);
 
 // Enter key adds todo
 todoInput?.addEventListener('keyup', (event) => {
-  if (event.keyCode === 13) {
+  if (event.key === 'Enter') {
     addTodo();
   }
 });
@@ -90,5 +124,11 @@ todoList?.addEventListener('click', (event) => {
     deleteTodo();
   }
 });
+
+document.querySelector('#generalButton')?.addEventListener('click', () => showCategory('general'));
+document.querySelector('#personalButton')?.addEventListener('click', () => showCategory('personal'));
+document.querySelector('#workButton')?.addEventListener('click', () => showCategory('work'));
+
+menuBtn.addEventListener('click', toggleMenu);
 
 showTodo();
